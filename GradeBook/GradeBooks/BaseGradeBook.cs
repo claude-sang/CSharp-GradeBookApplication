7b253017105f4c1dd8d0ca8915de2060523e9594
claude-sang <claude.sang@redrow.co.uk>
2018-10-04 10:19:10 +0100

    Completed the module

--- GradeBook/GradeBooks/BaseGradeBook.cs
+++ GradeBook/GradeBooks/BaseGradeBook.cs
@@ -13,6 +13,9 @@ namespace GradeBook.GradeBooks
     {
         public string Name { get; set; }
         public List<Student> Students { get; set; }
+        public GradeBookType Type { get => type; set => type = value; }
+
+        private GradeBookType type;
 
         public BaseGradeBook(string name)
         {
